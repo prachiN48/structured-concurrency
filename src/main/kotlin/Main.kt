0a811869319460e10c01fb03cc5582b135aac7e6
Prachi Nawathe <prachi.nawathe@thoughtworks.com>
2022-01-05 17:40:28 +0530

    extract coroutine into a private method so more examples can be run in similar way from main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,16 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
+    cancelParentJobAndRecursivelyChildJobIsCancelled()
+}
+
+private fun cancelParentJobAndRecursivelyChildJobIsCancelled() {
     runBlocking {
-        val parentJob = launch {
-            val childJob = launch {
+        val parentJob = launch(Dispatchers.Default) {
+            val childJob = launch(Dispatchers.Default) {
                 var count = 1
                 while (count <= 5) {
                     println("Count: $count")
